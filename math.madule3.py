1a45304d02d65e1f87502e51185082024730e4fc
rama020 <118764128+rama020@users.noreply.github.com>
2022-12-16 23:22:54 +0530

    Add files via upload

---
+++ math.madule3.py
@@ -0,0 +1,26 @@
+'''
+DATE:16TH DEC 2022
+DAY: FRIDAY
+TOPIC: MATH MODULE
+AUTHOR:RAMA BHARGAvi
+'''
+import math
+a=math.pow(3,3)
+b=3**3
+print(a,type(a))
+print(b,type(b))
+print(math.pi)
+print(math.pi*2)
+print(math.pi*3**2)
+print(math.sqrt(4))
+print(math.sqrt(6.7))
+print(math.sqrt(24))
+print(math.sqrt(25))
+c=2
+d=6
+e=14
+f=0
+print(math.isqrt(c))
+print(math.isqrt(d))
+print(math.isqrt(e))
+print(math.isqrt(f))
