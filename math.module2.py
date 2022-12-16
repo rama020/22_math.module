e7e95d8f35319e396bfa153e423733cd6112da6e
rama020 <118764128+rama020@users.noreply.github.com>
2022-12-16 23:01:35 +0530

    Add files via upload

---
+++ math.module2.py
@@ -0,0 +1,30 @@
+'''
+DATE:16TH DEC 2022
+DAY: FRIDAY
+TOPIC: MATH MODULE
+AUTHOR: RAMA BHARGAVI
+'''
+import math
+print(math.ceil(0.1))#1
+print(math.ceil(2.4))#3
+print(math.ceil(7.3))#8
+print(math.ceil(-0.3))#0
+print(math.ceil(-4.3))#-4
+print(math.ceil(22.6))#23
+print(math.ceil(14.1))#15
+print(math.exp(1))#2.718281828459045
+print(math.exp(3))#20.085536923187668
+print(math.factorial(0))#1
+print(math.factorial(1))#1
+print(math.factorial(2))#2
+print(math.factorial(3))#24
+print(math.factorial(4))#120
+print(math.factorial(5))#23
+print(math.floor(23.4))#23
+print(math.floor(23.5))#23
+print(math.floor(23.6))#-24
+print(math.floor(-23.6))#75
+print(math.ceil(75.0))#76
+print(math.ceil(75.3))
+print(math.floor(-19.1))
+print(math.ceil(75.5))#76
